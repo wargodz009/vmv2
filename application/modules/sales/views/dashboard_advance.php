bdc72c5bda66ff3fad8eda142d2e9a7fb1dc417f
wargodz009 <jayar_mundala@yahoo.com>
2015-09-07 16:11:42 +0800

    sales totals

--- application/modules/sales/views/dashboard_advance.php
+++ application/modules/sales/views/dashboard_advance.php
@@ -37,14 +37,19 @@
 	<td>Area</td>
 	<td>Msr Name</td>
 	<td>Sales</td>
+	<td>RGS</td>
+	<td>Net Sales of RGS</td>
 	<td>Quota</td>
 	<td>Sales %</td>
-	<td>RGS</td>
 	<!--td>End Month</td-->
 </thead>
 <tbody>
 <?php 
 	$total = 0;
+	$rgs_total = 0;
+	$net_rgs_total = 0;
+	$q_total = 0;
+	$sp_total = 0;
 	if(isset($sales) && !empty($sales)) {
 		foreach ($sales->result() as $msr) {
 			if($district == '00' || $district == $msr->district_id) {
@@ -54,9 +59,10 @@
 			<td><a href="<?=base_url();?>sales/area/<?=$msr->district_id;?>"><?=$this->crud_model->read('district',array(array('where','district_id',$msr->district_id)),'name');?></a></td>
 			<td><a href="<?=base_url();?>sales/all_sales/<?=$msr->user_id;?>"><?=strtoupper($msr->area) .' '. $msr->last_name.', '.$msr->first_name;?></a></td>
 			<td>P <?php $amount = modules::run('sales/get_sales',$msr->user_id,$month,$year); echo number_format($amount); $total = $total + $amount; ?></td>
-			<td>P <?=number_format($quota);?></td>
-			<td><?=number_format(($amount / $quota) * 100,2);?></td>
-			<td>P <?php $rgs_amount = modules::run('rgs/get_rgs',$msr->user_id,$month,$year); echo number_format($rgs_amount); ?></td>
+			<td>P <?php $rgs_amount = modules::run('rgs/get_rgs',$msr->user_id,$month,$year); echo number_format($rgs_amount); $rgs_net = $amount - $rgs_amount; $rgs_total += $rgs_amount; ?></td>
+			<td>P <?=$rgs_net; $net_rgs_total += $rgs_net; ?></td>
+			<td>P <?=number_format($quota); $q_total += $quota; ?></td>
+			<td><?php $s_percent = number_format(($amount / $quota) * 100,2); echo $s_percent; $sp_total +=$s_percent; ?></td>
 			<!--td><?php //echo modules::run('sales/get_sales',get_msr_client_id($msr->user_id),$month,$year); ?></td-->
 		</tr>
 <?php 			}
@@ -77,7 +83,10 @@
 		<td></td>
 		<td></td>
 		<td>Total: P <?=number_format($total);?></td>
-		<td></td>
+		<td>P <?=number_format($rgs_total);?></td>
+		<td>P <?=number_format($net_rgs_total);?></td>
+		<td>P <?=number_format($q_total);?></td>
+		<td>P <?=number_format($sp_total);?></td>
 	</tr></thead>
 </tbody>
 </table>
